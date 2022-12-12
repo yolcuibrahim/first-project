7e019b87545aabcf707f5749bca3bc332f6aad1c
yolcuibrahim <92376531+yolcuibrahim@users.noreply.github.com>
2022-12-12 23:19:28 +0300

    first compnent created.

---
+++ src/component/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+ 
+    return(
+    <div className = "nav" >
+        
+        <ul className="navbar">
+            <li>Home</li>
+            <li>Profile</li>
+            <li>About</li>
+        </ul>
+
+    </div>
+    )
+
+
+}
+
+export default Header;
